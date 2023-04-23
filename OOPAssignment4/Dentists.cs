2bdb76d8267b42f664d20da29cf70bc60b44e033
Ugyen Lama <ugyenchheda@gmail.com>
2023-04-24 00:13:19 +0300

    separated methods

--- OOPAssignment4/Dentists.cs
+++ OOPAssignment4/Dentists.cs
@@ -1,1 +1,79 @@
-﻿
+﻿using System.Data.SqlClient;
+using OOPAssignment4;
+class Dentist
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string TelNum { get; set; }
+}
+
+class Dentists
+{
+    public List<Dentist> GetAllDentists(SqlConnection connection)
+    {
+        List<Dentist> dentists = new List<Dentist>();
+
+        using (SqlCommand command = new SqlCommand("SELECT * FROM Dentist", connection))
+        using (SqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                dentists.Add(new Dentist
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    TelNum = reader.GetString(2)
+                });
+            }
+        }
+
+        return dentists;
+    }
+
+    public void InsertDentist(SqlConnection connection, Dentist dentist)
+    {
+        using (SqlCommand command = new SqlCommand("INSERT INTO Dentist (Name, TelNum) VALUES (@name, @telNum)", connection))
+        {
+            command.Parameters.AddWithValue("@name", dentist.Name);
+            command.Parameters.AddWithValue("@telNum", dentist.TelNum);
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public List<Dentist> FindDentist(SqlConnection connection, string name)
+    {
+        List<Dentist> dentists = new List<Dentist>();
+
+        using (SqlCommand command = new SqlCommand("SELECT * FROM Dentist WHERE Name LIKE '%' + @name + '%'", connection))
+        {
+            command.Parameters.AddWithValue("@name", name);
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    dentists.Add(new Dentist
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        TelNum = reader.GetString(2)
+                    });
+                }
+            }
+        }
+
+        return dentists;
+    }
+
+    public void UpdateDentist(SqlConnection connection, Dentist dentist)
+    {
+        using (SqlCommand command = new SqlCommand("UPDATE Dentist SET Name = @name, TelNum = @telNum WHERE Id = @id", connection))
+        {
+            command.Parameters.AddWithValue("@name", dentist.Name);
+            command.Parameters.AddWithValue("@telNum", dentist.TelNum);
+            command.Parameters.AddWithValue("@id", dentist.Id);
+            command.ExecuteNonQuery();
+        }
+    }
+
+
+}
